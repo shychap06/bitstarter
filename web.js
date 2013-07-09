ffe4750f83fa4111154cc6d814430f7edbe129b8
Kevin Ronson <example@kev.com>
2013-07-09 21:54:34 +0000

    Updated to read index file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
+var buffer = new Buffer(128);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFileSync("index.html", buffer);
+    response.send(buffer.toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
