b90df8f8e38779900232f2646c9163ff38c3af6c
Ubuntu <ubuntu@ip-172-31-2-5.us-west-2.compute.internal>
2013-07-22 21:30:57 +0000

    updated with images

--- web.js
+++ web.js
@@ -2,11 +2,21 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+
+app.configure(function(){
+    app.use(express.static(__dirname + "/img/site"));
+    }
+);
+
+
 app.get('/', function(request, response) {
     response.send(fs.readFileSync('index.html').toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+
+
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
